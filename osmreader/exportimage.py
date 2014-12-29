40b6681fa758b29580efc8e4f3ab7c44faf873ba
XeryusTC <armadillo@onenetbeyond.org>
2014-12-29 12:54:32 +0100

    Remove a lot of duplicate code

--- osmreader/exportimage.py
+++ osmreader/exportimage.py
@@ -11,15 +11,20 @@ class MapImageExporter:
             max_longitude, *args, node_color=(0, 0, 0), way_color="allrandom",
             bg_color="white", enlargement=50000):
         self.logger = logging.getLogger('mapbots.osmreader.exportimage.MapImageExporter')
+
         self.nodes = nodes
         self.ways = ways
-        self.node_color = node_color
-        self.way_color = way_color
+
         self.enlargement = enlargement
         self.width = math.ceil((max_longitude - min_longitude) * self.enlargement)
         self.height = math.ceil((max_latitude - min_latitude) * self.enlargement)
         self.min_longitude = min_longitude
         self.min_latitude = min_latitude
+
+        self.__colors = {}
+        self.node_color = node_color
+        self.way_color = way_color
+        self.bg_color = bg_color
 
     def export(self, filename="export.png"):
         self.logger.info('Exporting a map image to %s', filename)
@@ -41,51 +46,34 @@ class MapImageExporter:
 
         im.transpose(Image.FLIP_TOP_BOTTOM).save(filename)
 
-    @property
-    def node_color(self):
-        return self.__get_color(self._node_color)
-
-    @node_color.setter
-    def node_color(self, new):
-        self._node_color = self.__set_color(new)
-
-    @property
-    def way_color(self):
-        return self.__get_color(self._way_color)
-
-    @way_color.setter
-    def way_color(self, new):
-        self._way_color = self.__set_color(new)
-
-    @property
-    def bg_color(self):
-        return self.__get_color(self._bg_color)
-
-    @bg_color.setter
-    def bg_color(self, new):
-        self._way_color = self.__set_color(new)
-
-    def __get_color(self, color):
-        if color == None:
-            return(randrange(255), randrange(255), randrange(255))
-        else:
-            return color
-
-    def __set_color(self, new):
-        if isinstance(new, tuple) and len(new) == 3:
-            return new
-        elif isinstance(new, str):
-            new = new.lower()
-            if new == "random":
-                # Just pick a random color to use forever
-                return (randrange(255), randrange(255), randrange(255))
-            elif new == "allrandom":
-                # Use a random color every time
-                return None
+    def __getattr__(self, name):
+        try:
+            if self.__colors[name] == None:
+                return(randrange(255), randrange(255), randrange(255))
             else:
-                # Asume a valid color string
-                return new
-        elif new == None:
-            return None
+                return self.__colors[name]
+        except KeyError:
+            raise AttributeError("'{0}' object has no attribute '{1}'".format(self.__name__, name))
 
-        raise TypeError("Unexpected type received: {}".format(type(new)))
+    def __setattr__(self, name, value):
+        if name[-6:] == "_color":
+            if isinstance(value, tuple) and len(value) == 3:
+                self.__colors[name] = value
+            elif isinstance(value, str):
+                value = value.lower()
+                if value == "random":
+                    # Just pick a random color and use it forever
+                    self.__colors[name] = (randrange(255), randrange(255), randrange(255))
+                elif value == "allrandom":
+                    # Use a random color every time (None has special meaning in __getattr__)
+                    self.__colors[name] = None
+                else:
+                    # Asume the string is a valid color string
+                    self.__colors[name] = value
+            elif value == None:
+                self.__colors[name] = None
+            else:
+                raise TypeError("Unexpected color type received: {}".format(type(value)))
+        else:
+            # Use default behaviour for non-colors
+            self.__dict__[name] = value
