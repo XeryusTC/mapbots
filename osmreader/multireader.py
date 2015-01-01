57db46d978e919cc87fbc9bff4036d5de028414a
XeryusTC <armadillo@onenetbeyond.org>
2015-01-02 00:36:58 +0100

    Parse the XML elements as they are read.
    - Also update some of the documentation

--- osmreader/multireader.py
+++ osmreader/multireader.py
@@ -64,6 +64,17 @@ class MultiReader:
             self.load(filename)
 
     def load(self, filename):
+        """Parses data from an OSM file and stores it in memory.
+
+        Starts a subprocess that reads the XML and parses. It is then
+        passed back through a queue and handled. OSM Data gets stored in
+        class members.
+
+        Logging from the subprocess is also handled here.
+
+        Params:
+        filename - The file which holds the OSM XML
+        """
         if not isinstance(filename, str):
             raise TypeError("Filename must be the name of a file on an absolute or relative path")
         elif len(filename) == 0:
@@ -85,14 +96,35 @@ class MultiReader:
                 # Get the next element in the XML file
                 elem = elemqueue.get(False)
             except queue.Empty:
+                # Don't do anything if there are no elements to parse
                 pass
             else:
                 # Finish if there are no more elements to parse
                 if elem is None:
                     running = False
                 # Handle elements
+                else:
+                    if elem.tag == "bounds":
+                        self.min_latitude = float(elem.attrib['minlat'])
+                        self.max_latitude = float(elem.attrib['maxlat'])
+                        self.min_longitude = float(elem.attrib['minlon'])
+                        self.max_longitude = float(elem.attrib['maxlon'])
+                        self.logger.info("Area of map is defined by (%.4f, %.4f), (%.4f, %.4f)",
+                                         self.min_latitude, self.min_longitude,
+                                         self.max_latitude, self.max_longitude)
+                    elif elem.tag == "node":
+                        n = self._parse_node(elem)
+                        self.nodes[n.id] = n
+                    elif elem.tag == "way":
+                        try:
+                            w = self._parse_way(elem)
+                        except MultiReader.UnusedWayException:
+                            # This way is not used so don't register it
+                            pass
+                        else:
+                            self.ways[w.id] = w
 
-            # Handle queued log messages
+            # Handle (all) queued log messages
             try:
                 while True:
                     record = logqueue.get(False)
@@ -104,3 +136,63 @@ class MultiReader:
                 pass
         parser.join()
         self.logger.info("Finished multiprocess main loop")
+        self.logger.info("Found %d nodes", len(self.nodes))
+        self.logger.info("Found %d ways", len(self.ways))
+
+    def _parse_node(self, elem):
+        """Parses a node XML element to the Node class.
+
+        Params:
+        elem - an XML Element that has a node tag (does not check
+               whether it is actually a node tag)
+        """
+        n = Node(elem.attrib['id'], elem.attrib['lat'], elem.attrib['lon'])
+        n.tags = self._parse_tags(elem)
+        return n
+
+    def _parse_way(self, elem):
+        """Parses a way XML element to the Way class.
+
+        Raises an exception when a way is not a 'highway', buildings,
+        fields, lakes and others are all described by ways but they are
+        useless for pathfinding. The exception is meant to filter these.
+
+        Params:
+        elem - an XML Element that has a way tag (does not check whether
+               it is actually a way tag)
+        """
+        w = Way(elem.attrib['id'])
+        w.tags = self._parse_tags(elem)
+        if 'highway' not in w.tags.keys():
+            raise MultiReader.UnusedWayException
+        w.nodes = [int(node.attrib['ref']) for node in elem.findall('nd')]
+        return w
+
+    def _parse_tags(self, elem):
+        """Parses all 'tag' subtags of a XML Element
+
+        Is given a XML Element and parses all children which are a tag.
+        It tries to convert the value of the tag to int or float if
+        possible.
+
+        Params:
+        elem - an XML Element that possibly contains one or more 'tag'
+               child elements.
+        """
+        if elem.find("tag") == None:
+            return {}
+        ret = {}
+        for tag in elem.findall("tag"):
+            key = tag.attrib['k']
+            value = tag.attrib['v']
+            if value.isdigit():
+                ret[key] = int(value)
+            try:
+                ret[key] = float(value)
+            except:
+                ret[key] = value
+        return ret
+
+    class UnusedWayException(Exception):
+        """Used to indicate that a way element is useless for pathfinding."""
+        pass
