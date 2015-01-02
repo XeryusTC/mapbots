b3ec2e6e24abd8872c0051c0602825d90c49c009
XeryusTC <armadillo@onenetbeyond.org>
2015-01-02 18:51:29 +0100

    Optimise nodes and ways representation
    - Remove loggers from the classes so they can be instantiated faster
    - Make sure that arrays are always arrays to optimise memory usage

--- osmreader/elements.py
+++ osmreader/elements.py
@@ -5,19 +5,33 @@ logger = logging.getLogger('mapbots.osmreader.elements')
 
 class Node:
     def __init__(self, id, latitude, longitude):
-        self.logger = logging.getLogger('mapbots.osmreader.elements.Node')
         self.id = int(id)
         self.latitude = float(latitude)
         self.longitude = float(longitude)
         self.tags = {}
-        self.ways = array.array('q')
+        self.ways = []
+
+    @property
+    def ways(self):
+        return self._ways
+
+    @ways.setter
+    def ways(self, data):
+        self._ways = array.array('q', data)
 
     def __str__(self):
         return "{id} ({lat}, {lon})".format(id=self.id, lat=self.latitude, lon=self.longitude)
 
 class Way:
     def __init__(self, id):
-        self.logger = logging.getLogger('mapbots.osmreader.elements.Way')
         self.id = int(id)
         self.tags = {}
-        self.nodes = array.array('q')
+        self.nodes = []
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @nodes.setter
+    def nodes(self, data):
+        self._nodes = array.array('q', data)
