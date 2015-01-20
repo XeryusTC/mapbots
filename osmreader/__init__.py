13d9735c179e440001905f81cda821aa8f6f0ca3
XeryusTC <armadillo@onenetbeyond.org>
2015-01-20 21:25:29 +0100

    Remove old readers

--- osmreader/__init__.py
+++ osmreader/__init__.py
@@ -1,5 +1,3 @@
-from osmreader.xmlreader import XMLReader
-from osmreader.increader import IncrementalReader
 from osmreader.exportimage import MapImageExporter, graph_to_file
 from osmreader.multireader import MultiReader
 from osmreader.graphbuilder import DirectionalGraphBuilder
