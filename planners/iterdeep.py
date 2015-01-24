6d5a420e15ca04d81297ce89c73bd5ee8bbcc86f
XeryusTC <armadillo@onenetbeyond.org>
2015-01-24 16:22:18 +0100

    Base IDDFS depth on the path length
    - Remove a useless variable
    - Reorder logic to prevent wrong results
    - Depth now actually reflects the length of the path that has been found
    - Set the default minimum depth to 1 to prevent wrong paths

--- planners/iterdeep.py
+++ planners/iterdeep.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 from pygraph.classes.digraph import digraph
 
-def iterative_deepening(graph, start_node, goal_node, max_depth=64, min_depth=8):
+def iterative_deepening(graph, start_node, goal_node, max_depth=64, min_depth=1):
     """Find a route in a graph using iterative deepening.
 
     Uses a standard Depth First Search algorithm to find a path.
@@ -24,10 +24,11 @@ def iterative_deepening(graph, start_node, goal_node, max_depth=64, min_depth=8)
     logger = logging.getLogger('.'.join((__name__, 'iterative_deepening')))
     logger.info('Starting looking for a route from %s to %s using IDDFS (max depth: %d)',
                 start_node, goal_node, max_depth)
+
     total_time = time.perf_counter()
-    for lim in range(min_depth, max_depth):
+    for lim in range(min_depth, max_depth+1):
         start = time.perf_counter()
-        path = _iterdeep_rec(graph, start_node, goal_node, lim, 0)
+        path = _iterdeep_rec(graph, start_node, goal_node, lim)
         if path is not None:
             logger.info('Found a path by iterative deepening DFS in %f sec with length %d',
                         (time.perf_counter() - total_time), len(path))
@@ -35,7 +36,7 @@ def iterative_deepening(graph, start_node, goal_node, max_depth=64, min_depth=8)
         logger.info('Elapsed %f sec during iterative deepening DFS with limit %d',
                     (time.perf_counter() - start), lim)
 
-def _iterdeep_rec(graph, current_node, goal_node, max_depth, depth, path=[]):
+def _iterdeep_rec(graph, current_node, goal_node, max_depth, path=[]):
     """The Depth First Search algorithm used in iterative deepening
 
     Decreases the branching factor by assuming that each section will
@@ -53,19 +54,14 @@ def _iterdeep_rec(graph, current_node, goal_node, max_depth, depth, path=[]):
     path - The path that has been build from the start node up to but
            not including current_node
     """
-    neighbours = graph.neighbors(current_node)
-    # Base case 1, the goal node is in the list of neighbours, return
-    # success + the found path
-    if goal_node in neighbours:
-        return path + [current_node, goal_node]
-    # Base case 2, we have reached the maximum depth, return failure
-    elif depth > max_depth:
+    # Base case 1, we have reached the maximum depth, return failure
+    if len(path) >= max_depth:
         return None
 
-    # Recursive case, filter all the nodes so we only expand nodes at
-    # the opposite the end where we entered the section
+    neighbours = graph.neighbors(current_node)
+    # Filter all the nodes so we only expand nodes at the opposite the
+    # end where we entered the section
     if len(path) > 0:
-        # Find the node we entered on
         node_info = graph.node_attributes(current_node)
         prev_node = graph.node_attributes(path[-1])
         entered_start = node_info['start_node'] in (prev_node['start_node'], prev_node['end_node'])
@@ -79,12 +75,17 @@ def _iterdeep_rec(graph, current_node, goal_node, max_depth, depth, path=[]):
                 new_neighbours.append(neighbour)
         neighbours = new_neighbours
 
-    # Expand the neighbours
+    # Base case 2, the goal node is in the list of neighbours, return
+    # success + the found path
+    if goal_node in neighbours:
+        return path + [current_node, goal_node]
+
+    # Recursive case, expand the neighbours
     for neighbour in neighbours:
         # Skip this neighbour if it is already in the path we traversed
         if neighbour in path:
             continue
-        # Recursive call, return its result if it is successful
-        ret = _iterdeep_rec(graph, neighbour, goal_node, max_depth, depth+1, path + [current_node])
+
+        ret = _iterdeep_rec(graph, neighbour, goal_node, max_depth, path + [current_node])
         if ret is not None:
             return ret
